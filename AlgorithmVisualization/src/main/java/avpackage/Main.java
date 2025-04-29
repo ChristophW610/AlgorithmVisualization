c7e0442238debc39fad480a59af9be11007c50d7
Christoph <Christoph@CHRIS.fritz.box>
2025-04-29 20:28:41 +0200

    added JFrame

--- AlgorithmVisualization/src/main/java/avpackage/Main.java
+++ AlgorithmVisualization/src/main/java/avpackage/Main.java
@@ -1,10 +1,16 @@
 package avpackage;
 
+import javax.swing.JFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hallo Welt!");
+		
+		JFrame frame = new JFrame("Bubblesort Visualizer");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 600);
+		frame.setVisible(true);
+		
 	}
 
 }
