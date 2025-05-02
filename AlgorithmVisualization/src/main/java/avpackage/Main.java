3c25142afa7ab029bc9cfd2c174e1a981ebe9fcc
Christoph <Christoph@CHRIS.fritz.box>
2025-05-02 14:55:22 +0200

    implemented stepwise BubbleSort visualization

--- AlgorithmVisualization/src/main/java/avpackage/Main.java
+++ AlgorithmVisualization/src/main/java/avpackage/Main.java
@@ -2,15 +2,54 @@ package avpackage;
 
 import javax.swing.JFrame;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
+			
+		int[] array = new int[400];
+		Random rand = new Random();
+		for(int i = 0; i < array.length; i++) {
+			array[i] = rand.nextInt(550);
+		}
+		
 		
 		JFrame frame = new JFrame("Bubblesort Visualizer");
+		SortPanel panel = new SortPanel(array);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(800, 600);
+		frame.setSize(815, 610);
+		frame.add(panel);
 		frame.setVisible(true);
+		frame.setResizable(false);
+		
+		new Thread(() -> {
+			try {
+				bubbleSort(array, panel);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}).start();
 		
 	}
+	
+	private static void bubbleSort(int[] array, SortPanel panel) throws InterruptedException {
+		int temp;
+        for (int i = 1; i < array.length; i++) {
+            for (int j = 0; j < array.length - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+
+                    panel.repaint();
+
+                    Thread.sleep(1);
+                }
+            }
+        }
+	}
+	
 
 }
---
+++ AlgorithmVisualization/src/main/java/avpackage/SortPanel.java
@@ -0,0 +1,29 @@
+package avpackage;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class SortPanel extends JPanel {
+	private int[] array;
+	
+	public SortPanel(int[] array) {
+		this.array = array;
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.GRAY);
+		int barWidth = 2;
+		
+		for(int i = 0; i < array.length; i++) {
+			int x = i*barWidth;
+			int y = getHeight() - array[i];
+			int height = array[i];
+			g.fillRect(x, y, barWidth, height);
+		}
+	}
+	
+}
